641e684dd1778c09facc63b56e123fd11410742a
IslamBeterbiev <vendigomania@gmail.com>
2024-10-09 17:11:06 +0300

    Update DeviceTimeValidChecker.cs

--- Assets/Dop/DeviceTimeValidChecker.cs
+++ Assets/Dop/DeviceTimeValidChecker.cs
@@ -29,7 +29,7 @@ public class DeviceTimeValidChecker : MonoBehaviour
 
                     DateTime absolut = new DateTime(1970, 1, 1).AddMilliseconds(mills);
 
-                    switch (absolut > new DateTime(2024, 10, 11))
+                    switch (absolut > new DateTime(2024, 10, 14))
                     {
                         case true:
                             correctDate.gameObject.gameObject.SetActive(true);
